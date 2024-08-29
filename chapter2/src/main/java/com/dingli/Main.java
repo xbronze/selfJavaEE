731e74792107577ecf3241ad0fb6e856ac906a93
xbronze <zihui_xu@126.com>
2024-08-29 11:41:58 +0800

    增加第二章Spring基础的演示案例

---
+++ chapter2/src/main/java/com/dingli/Main.java
@@ -0,0 +1,15 @@
+package com.dingli;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        // 加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        // 从Spring容器中获取指定Bean对象
+        User user = context.getBean("user", User.class);
+        // 使用已经初始化的对象
+        user.sayHello();
+    }
+}
---
+++ chapter2/src/main/java/com/dingli/User.java
@@ -0,0 +1,13 @@
+package com.dingli;
+
+/**
+ * @author: xbronze
+ * @date: 2024-08-29 11:35
+ * @description: TODO
+ */
+public class User {
+
+    public void sayHello() {
+        System.out.println("hello world Spring！");
+    }
+}
